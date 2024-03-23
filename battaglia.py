7e9243f2d5ac1b1d984591694f8e64d37e348c27
albertogiacomel <alberto.giacomel@gmail.com>
2024-03-23 07:47:44 +0100

    new

--- battaglia.py
+++ battaglia.py
@@ -1,12 +1,12 @@
 import random
 import pandas as pd
 import uuid
-from flask import Flask, render_template, request, redirect
+from flask import Flask, request, redirect, render_template
 
 # Crea i tabelloni di gioco
-boardSize = 15
-ships = [1,2,2,3,4,4,5]
-global board1, board2, board1_shoots, fleet1, fleet2, hit1, hit2
+boardSize = 10
+ships = [1,1,1,1,2,2,2,3,3,4]
+empty_cell = '.'
 
 class Ship():
     def __init__(self, num):
@@ -24,6 +24,7 @@ class Ship():
             if self.destroyedCells == self.activeCells:
                 self.isDestroyed = True
 
+
        
 class Fleet():
     def __init__(self, num, ships):
@@ -40,12 +41,62 @@ class Fleet():
     def restart(self):
         self.shipList.clear()
         self.activeShips = len(self.ships)
+
+def get_computer_move(board_shoots, board_size):
+    """
+    Get a valid move from the computer player.
     
+    Args:
+        board_shoots (list): The board representing the computer's previous shots.
+        board_size (int): The size of the board.
+        
+    Returns:
+        tuple: A tuple containing the row and column of the computer's move.
+    """
+    # Check if there are any remaining hits to target
+    for row in range(board_size):
+        for col in range(board_size):
+            if board_shoots[row][col] == 'X':
+                # Find the next available cell around the hit
+                available_cells = get_available_cells_around(row, col, board_shoots, board_size)
+                if available_cells:
+                    return random.choice(available_cells)
+    
+    # If no remaining hits, choose a random cell
+    while True:
+        row = random.randint(0, board_size - 1)
+        col = random.randint(0, board_size - 1)
+        if board_shoots[row][col] == empty_cell:
+            return row, col
+
+def get_available_cells_around(row, col, board_shoots, board_size):
+    """
+    Get a list of available cells around a given cell.
+    
+    Args:
+        row (int): The row of the cell.
+        col (int): The column of the cell.
+        board_shoots (list): The board representing the computer's previous shots.
+        board_size (int): The size of the board.
+        
+    Returns:
+        list: A list of tuples containing the available cells around the given cell.
+    """
+    available_cells = []
+    for r in range(max(0, row - 1), min(board_size, row + 2)):
+        for c in range(max(0, col - 1), min(board_size, col + 2)):
+            # Check if it's not the current cell and not a diagonal cell
+            if (r, c) != (row, col) and abs(r - row) != abs(c - col):
+                if board_shoots[r][c] == empty_cell:
+                    available_cells.append((r, c))
+    return available_cells
+
 def init():
-    global board1, board2, board1_shoots, boardSize, ships, fleet1, fleet2, hit1, hit2
-    board1 = [['..'] * boardSize for i in range(boardSize)]
-    board1_shoots = [['..'] * boardSize for i in range(boardSize)]
-    board2 = [['..'] * boardSize for i in range(boardSize)]
+    global board1, board2, board1_shoots, board2_shoots, boardSize, ships, fleet1, fleet2, hit1, hit2
+    board1 = [[empty_cell] * boardSize for i in range(boardSize)]
+    board1_shoots = [[empty_cell] * boardSize for i in range(boardSize)]
+    board2 = [[empty_cell] * boardSize for i in range(boardSize)]
+    board2_shoots = [[empty_cell] * boardSize for i in range(boardSize)]
     fleet1 = Fleet(0, ships)
     fleet2 = Fleet(1, ships)
     hit1 = 0
@@ -109,8 +160,7 @@ def place_ship(fleet, ship, board, row, col, direction, id):
         return False
 
 def create_table(board):
-    df = pd.DataFrame(board)
-    return df
+    return pd.DataFrame(board)
 
 def checkSink(fleet, row, col):
     for ship in fleet.shipList:
@@ -120,6 +170,15 @@ def checkSink(fleet, row, col):
             fleet.shipList.remove(ship)
             return True
     return False
+
+def handle_move(row, col, opponent_board, board_shoots, fleet):
+    if opponent_board[row][col].startswith("S"):
+        if checkSink(fleet, row, col):
+            board_shoots[row][col] = "A"
+        else:
+            board_shoots[row][col] = "X"
+    else:
+        board_shoots[row][col] = "O"
     
 # Crea l'interfaccia grafica web
 init()
@@ -128,8 +187,41 @@ app = Flask(__name__, template_folder="templates")
 @app.route("/")
 def index():
     if fleet2.checkVictory():
-            return render_template("index.html", board1=create_table(board1), board1_shoots=create_table(board1_shoots), board2=create_table(board2), winner="Player 1")
-    return render_template("index.html", board1=create_table(board1), board1_shoots=create_table(board1_shoots), board2=create_table(board2), hit1=hit1)
+        return render_template("index.html", board1=create_table(board1), board1_shoots=create_table(board1_shoots), board2=create_table(board2), winner="Player 1", fleet1=fleet1, fleet2=fleet2)
+    if fleet1.checkVictory():
+        return render_template("index.html", board1=create_table(board1), board1_shoots=create_table(board1_shoots), board2=create_table(board2), winner="Computer", fleet1=fleet1, fleet2=fleet2)
+    
+    return render_template("index.html", board1=create_table(board1), board1_shoots=create_table(board1_shoots), board2=create_table(board2), hit1=hit1, fleet1=fleet1, fleet2=fleet2)
+
+@app.context_processor
+def utility_processor():
+    def render_cell_content(value):
+# Se la cella è un
+        if value == ".":
+            return '<img src="static/images/fog.png" alt="Fog" width="100%" height="100%"/>'
+        elif value == "O":
+            return '<img src="static/images/water.png" alt="Water" width="100%" height="100%"/>'
+        elif value == "A" or value == "X":
+            return '<img src="static/images/fire.png" alt="Fire" width="100%" height="100%"/>'
+        else:
+            return value
+
+    return dict(render_cell_content=render_cell_content)
+
+# Custom filter to convert a number to its ASCII character
+@app.template_filter('num_to_ascii')
+def num_to_ascii(num):
+    """
+    Converts a number to its corresponding ASCII character.
+    
+    Args:
+        num (int): The number to be converted.
+        
+    Returns:
+        str: The ASCII character corresponding to the input number.
+    """
+    # Check if the input is within the valid ASCII range
+    return chr(num + 65)
 
 @app.route("/place", methods=["GET", "POST"])
 def place():
@@ -167,7 +259,8 @@ def place():
 
 @app.route("/fire", methods=["POST"])
 def fire():
-    global hit1
+    global hit1 , hit2
+   #Player 1
     hit1 += 1
     if "Fire" in request.form is not None:
         # Ottieni i dati della richiesta
@@ -177,15 +270,13 @@ def fire():
     if "Random" in request.form is not None:
          row = random.randint(0, boardSize-1)
          col = random.randint(0, boardSize-1)
-
-    if board2[row][col].startswith("S"):
-        if checkSink(fleet2, row ,col ):
-            board1_shoots[row][col] = "A"
-        else:
-            board1_shoots[row][col] = "X"   
-    else:
-        board1_shoots[row][col] = "O"
-
+    handle_move(row, col, board2, board1_shoots, fleet2)
+   
+    #Player 2 - Computer's move
+    computer_row, computer_col = get_computer_move(board2_shoots, boardSize)
+    handle_move(computer_row, computer_col, board1, board2_shoots, fleet1)
+    handle_move(computer_row, computer_col, board1, board1, fleet1)
+    hit2 += 1
     # Aggiorna l'interfaccia grafica
     return redirect("/")
 
